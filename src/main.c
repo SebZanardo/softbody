e0f9a982cc27eec28f3d2c40328e24a51452d2c7
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-09-30 17:09:32 +1000

    janky web mouse position code and rendering code

--- src/main.c
+++ src/main.c
@@ -2,11 +2,40 @@
 #include "arena.h"
 #include "constants.h"
 #include "raylib.h"
-#include "rlgl.h"
 #include "softbody.h"
 
 
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+
+typedef struct {
+    float screen_width;
+    float screen_height;
+    float scale;
+    float offset_x;
+    float offset_y;
+} WindowParameters;
+
+#ifdef WEB
+#include <emscripten/emscripten.h>
+
+EM_JS(bool, IsMobile, (), {
+    return (
+        typeof navigator !== 'undefined' &&
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+            .test(navigator.userAgent)
+    );
+});
+#endif
+
+
+void handle_resize();
 unsigned modulo(int value, unsigned m);
+
+
+WindowParameters window;
+bool handheld;
 
 
 int main(void) {
@@ -17,6 +46,16 @@ int main(void) {
     HideCursor();
 
     SetRandomSeed(1);
+
+    #ifdef WEB
+        handheld = IsMobile();
+    #endif
+    handle_resize();
+
+    /*Image icon = LoadImage("src/resources/icon.png");*/
+    /*SetWindowIcon(icon);*/
+
+    RenderTexture2D target = LoadRenderTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
 
     // Temporary
     Texture2D eye_16x16 = LoadTexture("src/data/images/eye(16x16).png");
@@ -46,8 +85,20 @@ int main(void) {
     softbody_create_random(main_arena, softbodies, &active_softbodies);
 
     while (!WindowShouldClose()) {
+        if (IsWindowResized()) handle_resize();
+
+        Vector2 render_mouse_pos = GetMousePosition();
+
+        #ifdef WEB
+            if (!handheld) {
+                render_mouse_pos.x *= window.screen_width / WINDOW_WIDTH;
+                render_mouse_pos.y *= window.screen_height / WINDOW_HEIGHT;
+            }
+        #endif
+
         old_mouse_pos = mouse_pos;
-        mouse_pos = GetMousePosition();
+        mouse_pos.x = (render_mouse_pos.x - window.offset_x) / window.scale;
+        mouse_pos.y = (render_mouse_pos.y - window.offset_y) / window.scale;
 
         second_buffer = second_buffer == 1 ? 0 : 1;
 
@@ -90,7 +141,7 @@ int main(void) {
         }
 
         // RENDER
-        BeginDrawing();
+        BeginTextureMode(target);
         ClearBackground(BLACK);
 
         DrawRectangleLinesEx(border, 2, WHITE);
@@ -119,8 +170,29 @@ int main(void) {
             }
         }
 
-        DrawCircleV(mouse_pos, 8, GREEN);
+        EndTextureMode();
 
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawTexturePro(
+            target.texture,
+            (Rectangle){
+                0,
+                0,
+                (float)target.texture.width,
+                (float)-target.texture.height
+            },
+            (Rectangle){
+                window.offset_x,
+                window.offset_y,
+                (float)target.texture.width * window.scale,
+                (float)-target.texture.height * window.scale
+            },
+            (Vector2){0, 0},
+            0.0f,
+            WHITE
+        );
+        DrawCircleV(render_mouse_pos, 16, MAGENTA);
         EndDrawing();
     }
 }
@@ -131,4 +203,15 @@ unsigned modulo(int value, unsigned m) {
         mod += m;
     }
     return mod;
+}
+
+void handle_resize() {
+    window.screen_width = GetScreenWidth();
+    window.screen_height = GetScreenHeight();
+    window.scale = MIN(
+        window.screen_width / WINDOW_WIDTH,
+        window.screen_height / WINDOW_HEIGHT
+    );
+    window.offset_x = (window.screen_width - window.scale * WINDOW_WIDTH) / 2;
+    window.offset_y = (window.screen_height - window.scale * WINDOW_HEIGHT) / 2;
 }
